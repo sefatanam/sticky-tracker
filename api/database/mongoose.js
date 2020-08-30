f5f38cebb8757f30e3885dad41f0cc4b1110126d
Sefat Anam <37630292+sefatanam@users.noreply.github.com>
2020-08-31 01:05:06 +0600

    Update mongoose.js

--- api/database/mongoose.js
+++ api/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://cnq:rPKMMHEtEVZI3TTb@cluster0-audg9.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}).then(() => {
+mongoose.connect('MONGO_URL_HERE', {useNewUrlParser: true}).then(() => {
     console.log('Connect Successfully')
 }).catch((err) => {
     console.log(err);
